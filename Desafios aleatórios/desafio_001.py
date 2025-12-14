036f416585af0a793b7f21500273a9fd7c366321
Bruno Martins <bruno.eletr@gmail.com>
2025-12-13 22:01:31 -0300

    Implementação da solução do desafio 001

--- Desafios aleatórios/desafio_001.py
+++ Desafios aleatórios/desafio_001.py
@@ -1,3 +1,7 @@
+import doctest
+
+
+
 # ============================================
 # DESAFIO 1 — Normalização e Agregação de Logs
 # ============================================
@@ -60,3 +64,56 @@
 # - Sem prints
 # - Sem explicações
 # ============================================
+
+def aggregate_clicks(logs: list[str]) -> dict[str, int]:
+    """
+    Agrega valores de eventos do tipo "click" por usuário.
+
+    >>> logs = [
+    ...     "2024-01-10 10:00:00|u1|click|5",
+    ...     "2024-01-10 10:01:00|u2|view|3",
+    ...     "2024-01-10 10:02:00|u1|click|7",
+    ...     "2024-01-10 10:03:00|u3|click|2",
+    ...     "2024-01-10 10:04:00|u2|click|4"
+    ... ]
+    >>> aggregate_clicks(logs) == {'u1': 12, 'u2': 4, 'u3': 2}
+    True
+
+    >>> aggregate_clicks([])
+    {}
+
+    >>> aggregate_clicks([
+    ...     "2024-01-10 10:00:00|u1|view|5",
+    ...     "2024-01-10 10:01:00|u2|view|3"
+    ... ])
+    {}
+    """
+    lista_total_por_usuario = {}
+    for log in logs:
+        _, usuario, tipo, valor  = log.split("|")
+        if tipo == "click":
+            if lista_total_por_usuario.get(usuario):
+                lista_total_por_usuario[usuario] += int(valor)
+            else:
+                lista_total_por_usuario[usuario] = int(valor)
+
+    return lista_total_por_usuario
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    doctest.testmod(verbose=True)
+    logs = [
+    "2024-01-10 10:00:00|u1|click|5",
+    "2024-01-10 10:01:00|u2|view|3",
+    "2024-01-10 10:02:00|u1|click|7",
+    "2024-01-10 10:03:00|u3|click|2",
+    "2024-01-10 10:04:00|u2|click|4"]
+
+    print(aggregate_clicks(logs))
